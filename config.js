91b417e2cad289c1cee3ac719d1e3fef9c600be9
ZaheerAli12134 <zaheermirali121@gmail.com>
2025-10-31 20:57:38 +0000

    prod fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
-     window.location.hostname === '127.0.0.1'
+  window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
     ? 'http://localhost:3000/api'
-    : 'https://fiction-folder.onrender.com';
+    : 'https://fiction-folder.onrender.com/api';
 
 export async function getFirebaseConfig() {
   try {
@@ -9,10 +9,11 @@ export async function getFirebaseConfig() {
     if (!response.ok) throw new Error(`Fail: ${response.status}`);
     return await response.json();
   } catch (error) {
-    console.error(error);
-    alert('failed');
+    console.error('Failed to fetch Firebase config:', error);
+    alert('Failed to load Firebase config');
     throw error;
   }
 }
 
 export { API_BASE_URL };
+
